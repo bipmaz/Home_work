a376e05b95dbd3c1d8c3d62339a33e84971b207b
bipmaz <118344639+bipmaz@users.noreply.github.com>
2023-01-10 13:19:51 +0300

    Первое задание

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from goods_prob import*
+def generator(list_with_goods):
+    for str_good in list_with_goods:
+        list_good = str_good.split(':')
+        name = list_good[0]
+        price = list_good[1]
+        count = list_good[2]
+        production_date = list_good[3]
+        expiration_day = list_good[4]
+        yield good_list.add_good_in_list(Good(name, price, count, production_date, expiration_day)), good_list.get_mean_price()
 if __name__ == "__main__":
     good_list = GoodList()
 
     with open('prob.txt', 'r') as file:
         list_with_goods = file.readlines()
-
-        for str_good in list_with_goods:
-            list_good = str_good.split(':')
-
-            name = list_good[0]
-            price = list_good[1]
-            count = list_good[2]
-            production_date = list_good[3]
-            expiration_day = list_good[4]
-
-            good_list.add_good_in_list(Good(name, price, count, production_date, expiration_day))
-            good_list.get_mean_price()
+    for i in generator(list_with_goods):
+        i
